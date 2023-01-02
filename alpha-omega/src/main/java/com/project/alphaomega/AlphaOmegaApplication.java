26c0d38d09ab80e287290376f929389af0714932
leanna1223 <tojuny2312@gmail.com>
2023-01-02 12:28:20 +0900

    #1 spring project init.
    project init.

---
+++ alpha-omega/src/main/java/com/project/alphaomega/AlphaOmegaApplication.java
@@ -0,0 +1,13 @@
+package com.project.alphaomega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaOmegaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaOmegaApplication.class, args);
+    }
+
+}
---
+++ alpha-omega/src/test/java/com/project/alphaomega/AlphaOmegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.alphaomega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaOmegaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
