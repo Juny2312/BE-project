7268c25b6db825e88aa50b7436a2693c9cf2341c
leanna1223 <tojuny2312@gmail.com>
2023-01-02 20:03:59 +0900

    #4 Article Entity Checked
    -Checking the Persistence
    -Pattern Matching

--- alpha-omega/src/main/java/com/project/alphaomega/domain/Article.java
+++ alpha-omega/src/main/java/com/project/alphaomega/domain/Article.java
@@ -1,5 +1,6 @@
 package com.project.alphaomega.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,15 +12,18 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 @Getter
 @Slf4j
 @ToString
+@EqualsAndHashCode
 @Table(indexes = {
         @Index(columnList = "title" ),
-        @Index(columnList = "title" ),
-        @Index(columnList = "title" ),
-        @Index(columnList = "title" ),
+        @Index(columnList = "hashtag" ),
+        @Index(columnList = "createAt" ),
+        @Index(columnList = "createBy" ),
 
 
 })
@@ -30,24 +34,49 @@ public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Setter
+    @Setter @Column(nullable = false)
     private String title;
     @Setter
     private String hashtag;
-    @Setter
+    @Setter @Column(nullable = false, length = 10000)
     private String content;
 
 
-    @CreatedDate
+    @CreatedDate @Column(nullable = false)
     private LocalDateTime createAt;
-    @CreatedBy
+    @CreatedBy @Column(nullable = false, length = 100)
     private String createdBy;
 
-    @LastModifiedDate
+    @LastModifiedDate @Column(nullable = false, length = 100)
     private LocalDateTime modifiedAt;
 
-    @LastModifiedBy
+    @LastModifiedBy @Column(nullable = false, length = 100)
     private String modifiedBy;
 
 
+    protected Article(){} // Basic Constructor
+
+    private Article(String title, String hashtag, String content) {
+        this.title = title;
+        this.hashtag = hashtag;
+        this.content = content;
+    }
+
+    public static Article of(String title, String hashtag, String content){
+        return new Article(title,content,hashtag);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false; // Pattern Variable (Matching)
+        return id != null && id.equals(article.id); // Checking the
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
